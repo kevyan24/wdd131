98468c0a209806ea0fc2f7591ef0edde9a23a32d
kevyan24 <yan24010@byui.edu>
2025-02-11 03:46:07 -0500

    feat: lazy loading

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,10 @@
+const year = document.querySelector("#year");
+const lastModify = document.querySelector("#lastModify");
+
+const today = new Date();
+const theLastModify = document.lastModified;
+
+
+year.innerHTML = `&copy${today.getFullYear()} | Kevin Andres Yanez | Ecuador`;
+lastModify.innerHTML = `<span>Last Modification: ${theLastModify}</span>`;
+
